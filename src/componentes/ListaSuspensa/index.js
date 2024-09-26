4aaa81eb9000fcd6e6cdd3d8229d9d71415b0b81
pedrovmlopes <pedrovitormlopes@gmail.com>
2024-09-26 17:55:02 -0300

    Início da implementação do arquivamento de colaboradores

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,10 @@ const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
       <label>{props.label}</label>
-      <select>
+      <select
+        onChange={(evento) => props.aoAlterado(evento.target.value)}
+        required={props.obrigatorio}
+      >
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
